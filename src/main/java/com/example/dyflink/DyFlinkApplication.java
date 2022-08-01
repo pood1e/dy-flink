535324d506e69b94fbbe3a6d323eb4bb18296417
OvO-a <linchenwei@hikvision.com>
2022-08-01 21:41:35 +0800

    initial project

---
+++ src/main/java/com/example/dyflink/DyFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.dyflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DyFlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DyFlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dyflink/DyFlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dyflink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyFlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
